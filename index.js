1e1a9394a623e85cec8b94d65ed9f0b965d12a76
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-07-01 01:38:51 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const cors = require('cors');
-const app = express()
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3kvya.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected')
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from Simple Task Manager')
